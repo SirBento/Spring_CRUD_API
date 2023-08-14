3d84b752cc0dc30a34968188c830f0decdd201a8
Bernard Towindo <bernardtowindo38@gmail.com>
2023-08-14 19:46:30 +0200

    New Commit

---
+++ src/main/java/com/bento/Spring_CRUD_API/SpringCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.bento.Spring_CRUD_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApiApplication.class, args);
+	}
+
+}
